f2301523f236e1811e248f595ce4d65c180f646b
Thales Correa <thales@sum.net.br>
2021-04-04 02:30:29 -0300

    Add helper to data folder

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -52,7 +52,7 @@ namespace dotnet_rpg.Data
                 response.Message = "Usuário já existe.";
                 return response;
             }
-            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            Helper.CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
 
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
@@ -70,15 +70,6 @@ namespace dotnet_rpg.Data
                 return true;
             }
             return false;
-        }
-
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-        {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512())
-            {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
         }
 
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
